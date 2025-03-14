fa1b84f7fffa8c6aa6c03ce9c3566e3e87110b2f
alexa <alexa@LAPTOP-SMKPBBBG>
2025-03-14 10:31:23 +0000

    i hate avl trees

---
+++ DataStructuresTest.cs
@@ -0,0 +1,68 @@
+﻿using ShakaCoin.Blockchain;
+using ShakaCoin.Datastructures;
+using ShakaCoin.PaymentData;
+using System;
+
+namespace ShakaCoinTests
+{
+    [TestClass]
+    public class ShakaCoinTests
+    {
+        [TestMethod]
+        public void TestOutputBFNegative()
+        {
+            OutputBloomFilter obf = new OutputBloomFilter();
+            
+            for (int i = 0; i < 100; i++)
+            {
+                obf.AddItem(i.ToString());
+            }
+
+            Assert.IsFalse(obf.ProbablyContains(100.ToString()));
+        }
+
+        [TestMethod]
+        public void TestOutputBFTruePositive()
+        {
+            OutputBloomFilter obf = new OutputBloomFilter();
+
+            for (int i = 0; i < 100; i++)
+            {
+                obf.AddItem(i.ToString());
+            }
+
+            Assert.IsTrue(obf.ProbablyContains(99.ToString()));
+        }
+
+        private Transaction generateTransaction()
+        {
+            Transaction tx = new Transaction(0x00);
+            Random rnd = new Random();
+            int n = rnd.Next(0, int.MaxValue / 2);
+            tx.AddOutput(new Output((ulong)n, Hasher.Hash256(Hasher.GetBytesQuick(n.ToString()))));
+            return tx;
+        }
+
+        [TestMethod]
+        public void TestAVLTree()
+        {
+            TXNodeAVL root = new TXNodeAVL(new Transaction(0x00));
+
+            Transaction tx42 = new Transaction(0x00);
+
+            for (int i = 0;i < 100;i++)
+            {
+                Transaction tx = generateTransaction();
+                if (i==42)
+                {
+                    tx42 = tx;
+                }
+
+                Console.WriteLine(tx.CalculateFeeRate());
+                root.Insert(tx);
+            }
+
+            Assert.IsTrue(root.Contains(tx42));
+        }
+    }
+}
